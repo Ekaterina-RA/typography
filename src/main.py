77abb868d598df1fa00c8ae0ce81384375cdcf7d
Ekaterina <ekaterina.kuz@gmail.com>
2025-05-06 21:12:54 +0300

    main.py updated

--- src/main.py
+++ src/main.py
@@ -3,14 +3,33 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 
 class RequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        """Данный метод отвечает за обработку входящих запросов от пользователей"""
-        if self.path == "/" or self.path == "/contacts":
+        """Этот метод отвечает за обработку входящих запросов от пользователей"""
+        if self.path == "/home_page" or self.path == "/home_page.html":
             self.send_response(200)
             self.send_header("Content-type", "text/html; charset=utf-8")
             self.end_headers()
-            with open("index.html", "r", encoding="utf-8") as file:
+            with open("home_page.html", "r", encoding="utf-8") as file:
+                self.wfile.write(file.read().encode("utf-8"))
+        elif self.path == "/catalogue" or self.path == "/catalogue.html":
+            self.send_response(200)
+            self.send_header("Content-type", "text/html; charset=utf-8")
+            self.end_headers()
+            with open("catalogue.html", "r", encoding="utf-8") as file:
+                self.wfile.write(file.read().encode("utf-8"))
+        elif self.path == "/contacts" or self.path == "/contacts.html":
+            self.send_response(200)
+            self.send_header("Content-type", "text/html; charset=utf-8")
+            self.end_headers()
+            with open("contacts.html", "r", encoding="utf-8") as file:
+                self.wfile.write(file.read().encode("utf-8"))
+        elif self.path == "/category_1" or self.path == "/category_1.html":
+            self.send_response(200)
+            self.send_header("Content-type", "text/html; charset=utf-8")
+            self.end_headers()
+            with open("category_1.html", "r", encoding="utf-8") as file:
                 self.wfile.write(file.read().encode("utf-8"))
         else:
+            # Обработка 404 ошибки
             self.send_response(404)
             self.send_header("Content-type", "text/html; charset=utf-8")
             self.end_headers()
